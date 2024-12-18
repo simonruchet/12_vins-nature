addf81da419034d9c210da232aa64bb0e524373e
simonruchet <simon.ruchet@studentfr.ch>
2024-12-18 09:29:32 +0100

    élément fixé à gauche

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,18 @@ body.addEventListener(
   },
   { passive: false }
 );
+
+function setOffset() {
+  var wrapper = document.querySelector(".infos-fixe-wrapper");
+  var inner = document.querySelector(".infos-fixe");
+  var offsetRight =
+    window.innerWidth - wrapper.getBoundingClientRect().x + window.scrollX;
+
+  inner.style.left = `calc(100% - ${offsetRight}px)`;
+}
+
+setOffset();
+
+window.addEventListener("resize", () => {
+  setOffset();
+});
